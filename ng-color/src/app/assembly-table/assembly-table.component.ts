81f2aa9baa7638dce8ad879e1e2283584a6ab8f0
shan_alexander_newton <shan.newton@gmail.com>
2018-08-28 12:08:28 +0700

    adds randomizer function and table styling

--- ng-color/src/app/assembly-table/assembly-table.component.ts
+++ ng-color/src/app/assembly-table/assembly-table.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./assembly-table.component.css']
 })
 export class AssemblyTableComponent implements OnInit {
-  skills = this._skills.skills;
+  skills = this.generateRandomScenario(this._skills.skills);
 
   constructor( private _skills: SkillsService ) { }
-  ngOnInit() {}
+  ngOnInit() {
+  }
+
+  generateRandomScenario(skillsArr) {
+    for (let i = 0; i < skillsArr.length; i++) {
+      const currentSkill = skillsArr[i];
+      currentSkill.pipeline = Math.random() * (Math.random() * 1300);
+      currentSkill.notAssigned = currentSkill.pipeline - (currentSkill.pipeline * Math.random()) / 2;
+      currentSkill.assigned = currentSkill.pipeline - (currentSkill.pipeline * Math.random()) / 2;
+      // generate random numbers and conver to strings, ensure formatting
+      let mm: any = Math.round(Math.random() * 60);
+      (mm < 10) ? mm = '0' + mm : mm = '' + mm;
+      currentSkill.longestWaitingTime = '00:' + mm;
+
+      let hh: any = Math.round(Math.random() * 24);
+      (hh < 10) ? hh = '0' + hh : hh = '' + hh;
+      mm = Math.round(Math.random() * 60);
+      (mm < 10) ? mm = '0' + mm : mm = '' + mm;
+      currentSkill.nextDeadline = '' + hh + ':' + mm;
+
+      const sortingArraySplit = currentSkill.nextDeadline.split(':');
+      hh = parseInt(sortingArraySplit[0], 10) - Math.round(parseInt(sortingArraySplit[0], 10) * Math.random());
+      (hh < 10) ? hh = '0' + hh : hh = '' + hh;
+      currentSkill.sorting = '' + hh + ':' + Math.round(Math.random() * 60);
+
+      currentSkill.editors = Math.round(Math.random() * 20);
+    }
+    return skillsArr;
+  }
 
 }
