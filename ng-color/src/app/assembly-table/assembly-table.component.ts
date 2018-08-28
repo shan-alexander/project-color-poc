c16600aa02904f843d602cebf2df055898ddc13b
shan_alexander_newton <shan.newton@gmail.com>
2018-08-28 16:06:20 +0700

    adds rando bottleneck generator

--- ng-color/src/app/assembly-table/assembly-table.component.ts
+++ ng-color/src/app/assembly-table/assembly-table.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./assembly-table.component.css']
 })
 export class AssemblyTableComponent implements OnInit {
-  skills = this.generateRandomScenario(this._skills.skills);
+  zeSkills = this.generateRandomScenario(this._skills.skills);
+  skills = this.generateRandomEditorDeficiencyBottleneck(this.zeSkills);
 
   constructor( private _skills: SkillsService ) { }
   ngOnInit() {
@@ -35,9 +36,65 @@ export class AssemblyTableComponent implements OnInit {
       (hh < 10) ? hh = '0' + hh : hh = '' + hh;
       currentSkill.sorting = '' + hh + ':' + Math.round(Math.random() * 60);
 
-      currentSkill.editors = Math.round(Math.random() * 20);
+      currentSkill.editors = Math.round(Math.random() * 40);
+
+      // currentSkill.potential = ( (Math.random() * 10) > 9 ) ? 0 : ( ((Math.random() * 10) > 8) ? currentSkill.editors +
+      // Math.round(Math.random() * 0) : currentSkill.editors/2 + Math.round(Math.random() * 0) );
+      currentSkill.forced = (Math.round(Math.random() * 100) > 5) ? 0 : Math.round(Math.random() * 10);
+      currentSkill.idle = (Math.round(Math.random() * 100) > 10) ? 0 : Math.round(Math.random() * 10);
+      // hours randomizing below
+      currentSkill.zeroHours = ((Math.random() * 100 > 10) ? 0 : Math.round(Math.random() * 50));
+      currentSkill.twoHours = (Math.random() * 100 > 10) ?
+        currentSkill.zeroHours : (currentSkill.zeroHours + (Math.round(Math.random() * 30)));
+      currentSkill.fourHours = (Math.random() * 100 > 10) ?
+        currentSkill.twoHours : (currentSkill.twoHours + (Math.round(Math.random() * 30)));
+      currentSkill.eightHours = (Math.random() * 100 > 30) ?
+        currentSkill.fourHours : (currentSkill.fourHours + (Math.round(Math.random() * 30)));
+      currentSkill.twelveHours = (Math.random() * 100 > 75) ?
+        currentSkill.eightHours : (currentSkill.eightHours + (Math.round(Math.random() * 150)));
+      currentSkill.sixteenHours = (Math.random() * 100 > 75) ?
+        currentSkill.twelveHours : (currentSkill.twelveHours + (Math.round(Math.random() * 150)));
+      currentSkill.twentyFourHours = (Math.random() * 100 > 50) ?
+        currentSkill.sixteenHours : (currentSkill.sixteenHours + (Math.round(Math.random() * 1000)));
+      currentSkill.fourtyEightHours = (Math.random() * 100 > 50) ?
+        currentSkill.twentyFourHours : (currentSkill.twentyFourHours + (Math.round(Math.random() * 1000)));
+      currentSkill.seventyTwoHours = (Math.random() * 100 > 20) ?
+        currentSkill.fourtyEightHours : (currentSkill.fourtyEightHours + (Math.round(Math.random() * 555)));
     }
     return skillsArr;
   }
+
+  generateRandomEditorDeficiencyBottleneck(skillsArr) {
+    const rando = Math.floor(Math.random() * skillsArr.length);
+    const randoSkill = skillsArr[rando];
+    console.log('Bottleneck generated on ' + randoSkill.name);
+    randoSkill.editors = Math.round(Math.random() * 10);
+    randoSkill.assigned = Math.round(randoSkill.editors * (1.2 + Math.random()) );
+
+    let hh: any = Math.round(Math.random() * 2.7);
+      (hh < 10) ? hh = '0' + hh : hh = '' + hh;
+
+    let mm: any = Math.round(Math.random() * 60);
+      (mm < 10) ? mm = '0' + mm : mm = '' + mm;
+
+    randoSkill.nextDeadline = '' + hh + ':' + mm;
+    randoSkill.sorting = '-01:' + mm;
+    randoSkill.zeroHours = Math.round(Math.random() * 300);
+    randoSkill.twoHours = Math.round(Math.random() * 300);
+    randoSkill.fourHours = randoSkill.twoHours + Math.round(Math.random() * 300);
+    randoSkill.eightHours = randoSkill.fourHours;
+    randoSkill.twelveHours = randoSkill.eightHours;
+    randoSkill.sixteenHours = randoSkill.twelveHours + Math.round(Math.random() * 200);
+    randoSkill.twentyFourHours = randoSkill.sixteenHours;
+    randoSkill.fourtyEightHours = randoSkill.sixteenHours;
+    randoSkill.seventyTwoHours = randoSkill.sixteenHours + Math.round(Math.random() * 333);
+    randoSkill.notAssigned = randoSkill.eightHours - randoSkill.assigned;
+    randoSkill.pipeline = randoSkill.seventyTwoHours - randoSkill.assigned;
+    return skillsArr;
+  }
+
+  // if editors and potential == 0, then potential is red
+
+
 
 }
